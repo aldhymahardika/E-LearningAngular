7ad6335eb9d8f111cf1b832232d88f0e90e28ea9
Srirahayu07 <07.srirahayu@gmail.com>
2020-05-06 00:17:33 +0700

    e-learning v.2.3 update

--- src/app/pages/report-jadwal/report-jadwal.component.ts
+++ src/app/pages/report-jadwal/report-jadwal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/service/app.service';
 import { ReportJadwal } from 'src/app/layouts/model/reportJadwal';
+import { parse } from 'querystring';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-report-jadwal',
@@ -10,6 +12,9 @@ import { ReportJadwal } from 'src/app/layouts/model/reportJadwal';
 export class ReportJadwalComponent implements OnInit {
   jadwal = new ReportJadwal()
   reportAll:any[]
+  date:Date = null
+  data1:string
+  data2:string
   constructor(private uploadService: AppService) {
     this.getReportAllJadwal()
    }
@@ -26,9 +31,15 @@ export class ReportJadwalComponent implements OnInit {
   }
 
   getReportJadwal(){
+    var datePipe = new DatePipe('en-US')
+    this.data1= datePipe.transform(this.jadwal.tgl1, 'yyyy-MM-dd')
+    this.data2= datePipe.transform(this.jadwal.tgl2, 'yyyy-MM-dd')
+    // console.log(this.data1);
+    
     // var momentVariable = moment(this.jadwal.tgl1, 'MM-DD-YYYY');
     // var stringvalue = momentVariable.format('YYYY-MM-DD');
-    this.uploadService.getReportJadwal(this.jadwal.tgl1, this.jadwal.tgl2).subscribe(data=>{
+    // this.date = parse(this.jadwal.tgl1, 'YYYY-MM-DD', new Date())
+    this.uploadService.getReportJadwal(this.data1, this.data2).subscribe(data=>{
       this.reportAll=data
     })
   }
